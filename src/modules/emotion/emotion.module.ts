bdb818686ae9c23ef48fdadb621754724c4874ba
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-02-29 13:10:42 -0500

    feat: create two endpoints to get all emotions and get emotion by name

--- src/modules/emotion/emotion.module.ts
+++ src/modules/emotion/emotion.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrimaryEmotionService } from './services/primaryEmotion.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PrimaryEmotion } from './entities/primaryEmotion.entity';
+import { EmotionController } from './controllers/emotion.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([PrimaryEmotion])],
+  providers: [PrimaryEmotionService],
+  controllers: [EmotionController],
+})
 export class EmotionModule {}
